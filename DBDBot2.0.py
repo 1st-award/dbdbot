160f40c8e1e99aae90a78fa7aeeb23486a7a89c3
gomteang2dayo <54760813+gomteang2dayo@users.noreply.github.com>
2019-12-03 19:49:04 +0900

    Update DBDBot2.0.py

--- DBDBot2.0.py
+++ DBDBot2.0.py
@@ -30,7 +30,7 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name=".도움말   :D", type=0))
 	
     lastest = 'Null'
-    lastest_Perk = 'Null'
+    lastest_perk = 'Null'
 
     ch_name1 = os.environ["ch1"]
     ch_name2 = os.environ["ch2"]
