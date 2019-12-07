05de083073c1d145307438abf49596f8ff7995ee
gomteang2dayo <54760813+gomteang2dayo@users.noreply.github.com>
2019-12-07 15:58:11 +0900

    Update DBDBot2.0.py

--- DBDBot2.0.py
+++ DBDBot2.0.py
@@ -20,23 +20,23 @@ client = discord.Client()
 async def on_ready():
     print('Logged in as \nName: {}\n  ID: {}\n'.format(bot.user.name, bot.user.id))
     
-    a = bot.guilds
     print('Guild list')
-    
-    for i in a:
-    	path = '/app/serv/' + format(i) + '.py'
-    	print('{}'.format(i))
-    	shutil.copy('/app/serv/sample.py', path)
-    	
+    for guild in bot.guilds:
+        path = '/app/serv/' + format(guild.id) + '.py'
+        print('{}({})'.format(guild.name, guild.id))
+        if os.path.exists(path)==True: pass
+        else:
+            shutil.copy('/app/serv/sample.py', path)
+        
     print('='*10)
     await bot.change_presence(activity=discord.Game(name=".도움말   :D", type=0))
-	
+    
     lastest = 'Null'
     lastest_perk = 'Null'
     ch_name1 = os.environ["ch1"]
     ch_name2 = os.environ["ch2"]
     ch_name3 = os.environ["ch3"]
-	
+    
     channel = bot.get_channel(int(ch_name1))
     channel1 = bot.get_channel(int(ch_name2))
     channel2 = bot.get_channel(int(ch_name3))
@@ -52,7 +52,7 @@ async def on_ready():
         url = title.find('a', href = True)
         url1 = str(url['href'])
         
-        if lastest not url1:
+        if lastest not in url1:
             lastest = url1
             await channel.send('NEW!! Update!!\n' + str(title.string) + '\n' + url1)
             await channel1.send('NEW!! Update!!\n' + str(title.string) + '\n' + url1)
@@ -67,7 +67,7 @@ async def on_ready():
         
         link = 'https://cafe.naver.com/deadbydaylight/' + day[1:]
     
-        if lastest_perk not day:
+        if lastest_perk not in day:
             lastest_perk = day
         
             await channel.send('Perk!! Update!!\n' + link)
@@ -107,7 +107,7 @@ async def 살인마(ctx):
         id = (ctx.message.author.mention)
         msg = ctx.message.content
         msg = msg.replace(".살인마 ", "")
-        channel = format(ctx.message.guild)
+        channel = format(ctx.message.id)
         path = 'serv/'+channel + '.py'
         kpath = '/app/kadon/'
         size = (128, 128)
@@ -117,94 +117,94 @@ async def 살인마(ctx):
         
         
         if msg in '퍽':
-        	out = str(random.sample(perk.killer, 4))
-        	out = out.strip('[]')
-        	out = out.replace("'", "", 8)
-        	await ctx.send(format(id)  + "님의 살인마 퍽은 " + "**```cs\n" + str(out) + "입니다.\n```**")
+            out = str(random.sample(perk.killer, 4))
+            out = out.strip('[]')
+            out = out.replace("'", "", 8)
+            await ctx.send(format(id)  + "님의 살인마 퍽은 " + "**```cs\n" + str(out) + "입니다.\n```**")
         
         else:
-        	out = random.sample(character.killer, 2)
-        	killer = out
-        	out = str(out).strip('[]')
-        	out = out.replace("'", "", 4)
-        	await ctx.send( format(id) + "님의 살인마는 " + "**```cs\n" + str(out)  +"입니다.\n```**")
-        	
-        	if '아이템 = 1' in open(path).read():
-        		for i in killer:
-        			
-        			if i in '덫구':
-        				kpath = kpath + '덫구/t'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '종구':
-        				kpath = kpath + '종구/w'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '톱구':
-        				kpath = kpath + '톱구/h'
-        				a2 = random.sample(range(1, 19), 2)
-        			elif i in '너스':
-        				kpath = kpath + '너스/n'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '헤그':
-        				kpath = kpath + '헤그/he'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '전구':
-        				kpath = kpath + '전구/d'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '토구':
-        				kpath = kpath + '토구/t'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '빵구':
-        				kpath = kpath + '빵구/c'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '식구':
-        				kpath = kpath + '식구/sh'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '준구':
-        				kpath = kpath + '준구/ca'
-        				a2 = random.sample(range(1, 19), 2)
-        			elif i in '몽구':
-        				kpath = kpath + '몽구/n'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '직구':
-        				kpath = kpath + '직구/p'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '스피릿':
-        				kpath = kpath + '스피릿/sp'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '군단':
-        				kpath = kpath + '군단/l'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '역병':
-        				kpath = kpath + '역병/p'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '고스트':
-        				kpath = kpath + '고스트/go'
-        				a2 = random.sample(range(1, 20), 2)
-        			elif i in '데모고르곤':
-        				kpath = kpath + '데모고르곤/de'
-        				a2 = random.sample(range(1, 20), 2)
-        			
-        			bg = Image.open('/app/kadon/bg.jpg')
-        			img_item = Image.open(kpath +".jpg")
-        			img_add_on = Image.open(kpath+str(a2[0])+".jpg")
-        			img_add_on2 = Image.open(kpath+str(a2[1])+".jpg")
-        			img_item = img_item.convert("RGB")
-        			img_add_on = img_add_on.convert("RGB")
-        			bg = bg.convert("RGB")
-        			
-        			bg.thumbnail(size3)
-        			img_item.thumbnail(size)
-        			img_add_on.thumbnail(size2)
-        			img_add_on2.thumbnail(size2)
-        			
-        			bg.paste(img_item, (0, 0))
-        			bg.paste(img_add_on, (128, 0))
-        			bg.paste(img_add_on2, (128, 64))
-        			bg.save("/app/kadon/perk.jpg")
-        			result = "/app/kadon/perk.jpg"
-        			
-        			await ctx.send(file=discord.File(result))
-        			kpath = '/app/kadon/'
+            out = random.sample(character.killer, 2)
+            killer = out
+            out = str(out).strip('[]')
+            out = out.replace("'", "", 4)
+            await ctx.send( format(id) + "님의 살인마는 " + "**```cs\n" + str(out)  +"입니다.\n```**")
+            
+            if '아이템 = 1' in open(path).read():
+                for i in killer:
+                    
+                    if i in '덫구':
+                        kpath = kpath + '덫구/t'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '종구':
+                        kpath = kpath + '종구/w'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '톱구':
+                        kpath = kpath + '톱구/h'
+                        a2 = random.sample(range(1, 19), 2)
+                    elif i in '너스':
+                        kpath = kpath + '너스/n'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '헤그':
+                        kpath = kpath + '헤그/he'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '전구':
+                        kpath = kpath + '전구/d'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '토구':
+                        kpath = kpath + '토구/t'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '빵구':
+                        kpath = kpath + '빵구/c'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '식구':
+                        kpath = kpath + '식구/sh'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '준구':
+                        kpath = kpath + '준구/ca'
+                        a2 = random.sample(range(1, 19), 2)
+                    elif i in '몽구':
+                        kpath = kpath + '몽구/n'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '직구':
+                        kpath = kpath + '직구/p'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '스피릿':
+                        kpath = kpath + '스피릿/sp'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '군단':
+                        kpath = kpath + '군단/l'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '역병':
+                        kpath = kpath + '역병/p'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '고스트':
+                        kpath = kpath + '고스트/go'
+                        a2 = random.sample(range(1, 20), 2)
+                    elif i in '데모고르곤':
+                        kpath = kpath + '데모고르곤/de'
+                        a2 = random.sample(range(1, 20), 2)
+                    
+                    bg = Image.open('/app/kadon/bg.jpg')
+                    img_item = Image.open(kpath +".jpg")
+                    img_add_on = Image.open(kpath+str(a2[0])+".jpg")
+                    img_add_on2 = Image.open(kpath+str(a2[1])+".jpg")
+                    img_item = img_item.convert("RGB")
+                    img_add_on = img_add_on.convert("RGB")
+                    bg = bg.convert("RGB")
+                    
+                    bg.thumbnail(size3)
+                    img_item.thumbnail(size)
+                    img_add_on.thumbnail(size2)
+                    img_add_on2.thumbnail(size2)
+                    
+                    bg.paste(img_item, (0, 0))
+                    bg.paste(img_add_on, (128, 0))
+                    bg.paste(img_add_on2, (128, 64))
+                    bg.save("/app/kadon/perk.jpg")
+                    result = "/app/kadon/perk.jpg"
+                    
+                    await ctx.send(file=discord.File(result))
+                    kpath = '/app/kadon/'
 
         
 @bot.command(pass_context=True)
@@ -214,43 +214,43 @@ async def 생존자(ctx):
         msg = msg.replace(".생존자 ", "")
         
         if msg in "퍽":
-        	out = str(random.sample(perk.surviver, 4))
-        	out = out.strip('[]')
-        	out = out.replace("'", "", 8)
-        	await ctx.send(format(id) + "님의 생존자 퍽은 " + "**```cs\n" + str(out) +"입니다.\n```**") 
-        	
+            out = str(random.sample(perk.surviver, 4))
+            out = out.strip('[]')
+            out = out.replace("'", "", 8)
+            await ctx.send(format(id) + "님의 생존자 퍽은 " + "**```cs\n" + str(out) +"입니다.\n```**") 
+            
         else:
-        	out = str(random.sample(character.surviver, 2))
-        	out = out.strip('[]')
-        	out = out.replace("'", "", 4)
-        	await ctx.send( format(id) + "님의 생존자는 " + "**```cs\n" + str(out)  +"입니다.\n```**") 
+            out = str(random.sample(character.surviver, 2))
+            out = out.strip('[]')
+            out = out.replace("'", "", 4)
+            await ctx.send( format(id) + "님의 생존자는 " + "**```cs\n" + str(out)  +"입니다.\n```**") 
         
 @bot.command(pass_context=True)
 async def 리(ctx):
         await ctx.send("==========================================")
 @bot.command(pass_context=True)
 async def 퍽(ctx):
-	try:
-		msg = ctx.message.content
-		msg = msg.replace(".퍽 ", "")
-		id = (ctx.message.author.mention)
-		
-		msg1 = msg + ' ' + '1'
-		pn = msg1.split(' ')
-		
-		if pn[1] in '1':
-			path = '/app/killerimg/' + pn[0] + '.png'
-		elif pn[2] in '1':
-			path = '/app/killerimg/' + pn[0] + pn[1] + '.png'
-		elif pn[3] in '1':
-			path = '/app/killerimg/' + pn[0] + pn[1] + pn[2] + '.png'
-		elif pn[4] in '1':
-			path = '/app/killerimg/' + pn[0] + pn[1] + pn[2] + pn[3] + '.png'
-			
-		await ctx.send(file=discord.File(path))
-	
-	except FileNotFoundError:
-		await ctx.send(format(id) + '\n ' +msg+'퍽을 찾을 수 없습니다.')
+    try:
+        msg = ctx.message.content
+        msg = msg.replace(".퍽 ", "")
+        id = (ctx.message.author.mention)
+        
+        msg1 = msg + ' ' + '1'
+        pn = msg1.split(' ')
+        
+        if pn[1] in '1':
+            path = '/app/killerimg/' + pn[0] + '.png'
+        elif pn[2] in '1':
+            path = '/app/killerimg/' + pn[0] + pn[1] + '.png'
+        elif pn[3] in '1':
+            path = '/app/killerimg/' + pn[0] + pn[1] + pn[2] + '.png'
+        elif pn[4] in '1':
+            path = '/app/killerimg/' + pn[0] + pn[1] + pn[2] + pn[3] + '.png'
+            
+        await ctx.send(file=discord.File(path))
+    
+    except FileNotFoundError:
+        await ctx.send(format(id) + '\n ' +msg+'퍽을 찾을 수 없습니다.')
 
 @bot.command(pass_context=True)
 async def 살인마퍽(ctx):
@@ -264,7 +264,7 @@ async def 살인마퍽(ctx):
 @bot.command(pass_context=True)
 async def 생존자퍽(ctx):
         id = (ctx.message.author.mention)        
-        channel = format(ctx.message.guild)
+        channel = format(ctx.message.id)
         path = '/app/serv/' + channel + '.py'
         
         out = str(random.sample(perk.surviver, 4))
@@ -273,106 +273,106 @@ async def 생존자퍽(ctx):
         await ctx.send(format(id) + "님의 생존자는 " + "**```cs\n" + str(out) +"입니다.\n```**")
         
         if '아이템 = 1' in open(path).read():
-        	path = "/app/item/"
-        	size = (128, 128)
-        	size2 = (64, 64)
-        	size3 = (192, 128)
-        	
-        	a1 = random.choice(item.item)
-        	temp = a1[:1]
-        	
-        	if temp in 't':
-        		a2 = random.sample(item.ta, 2)
-        	elif temp in 'l':
-        		a2 = random.sample(item.la, 2)
-        	elif temp in 'h':
-        		a2 = random.sample(item.ha, 2)
-        	elif temp in 'm':
-        		a2 = random.sample(item.ma, 2)
-        	elif temp in 'k':
-        		a2 = random.sample(item.ka, 2)
-        	elif temp in 'f':
-        		a2 = "bg", "bg"
-        	bg = Image.open(path+'bg.jpg')
-        	img_item = Image.open(path+a1+".jpg")
-        	img_add_on = Image.open(path+a2[0]+".jpg")
-        	img_add_on2 = Image.open(path+a2[1]+".jpg")
-        	img_item = img_item.convert("RGB")
-        	img_add_on = img_add_on.convert("RGB")
-        	bg = bg.convert("RGB")
-        	
-        	bg.thumbnail(size3)
-        	img_item.thumbnail(size)
-        	img_add_on.thumbnail(size2)
-        	img_add_on2.thumbnail(size2)
-        	
-        	bg.paste(img_item, (0, 0))
-        	bg.paste(img_add_on, (128, 0))
-        	bg.paste(img_add_on2, (128, 64))
-        	bg.save(path+"perk.jpg")
-        	result = path+"perk.jpg"
-		
-        	await ctx.send(file=discord.File(result))
+            path = "/app/item/"
+            size = (128, 128)
+            size2 = (64, 64)
+            size3 = (192, 128)
+            
+            a1 = random.choice(item.item)
+            temp = a1[:1]
+            
+            if temp in 't':
+                a2 = random.sample(item.ta, 2)
+            elif temp in 'l':
+                a2 = random.sample(item.la, 2)
+            elif temp in 'h':
+                a2 = random.sample(item.ha, 2)
+            elif temp in 'm':
+                a2 = random.sample(item.ma, 2)
+            elif temp in 'k':
+                a2 = random.sample(item.ka, 2)
+            elif temp in 'f':
+                a2 = "bg", "bg"
+            bg = Image.open(path+'bg.jpg')
+            img_item = Image.open(path+a1+".jpg")
+            img_add_on = Image.open(path+a2[0]+".jpg")
+            img_add_on2 = Image.open(path+a2[1]+".jpg")
+            img_item = img_item.convert("RGB")
+            img_add_on = img_add_on.convert("RGB")
+            bg = bg.convert("RGB")
+            
+            bg.thumbnail(size3)
+            img_item.thumbnail(size)
+            img_add_on.thumbnail(size2)
+            img_add_on2.thumbnail(size2)
+            
+            bg.paste(img_item, (0, 0))
+            bg.paste(img_add_on, (128, 0))
+            bg.paste(img_add_on2, (128, 64))
+            bg.save(path+"perk.jpg")
+            result = path+"perk.jpg"
+        
+            await ctx.send(file=discord.File(result))
 
 @bot.command(pass_context=True)
 async def 아이템(ctx):
-	channel = format(ctx.message.guild)
-	path = '/app/serv/' + channel + '.py'
-	output = []
-	if '아이템 = 1' in open(path).read():
-		  f=open(path)
-		  
-		  for line in f:
-		  	if not '아이템 = 1' in line:
-		  		output.append(line)
-		  f.close()
-		  f = open(path, 'w')
-		  f.writelines(output)
-		  f.write('아이템 = 0')
-		  f.close()
-		  
-		  await ctx.send('아이템 모드가 꺼졌습니다.')
-	elif '아이템 = 0' in open(path).read():
-		f = open(path)
-		
-		for line in f:
-			if not '아이템 = 0' in line:
-				output.append(line)
-		f.close()
-		f = open(path, 'w')
-		f.writelines(output)
-		f.write('아이템 = 1')
-		f.close()
-		
-		await ctx.send('아이템 모드가 켜졌습니다.')
-		
+    channel = format(ctx.message.id)
+    path = '/app/serv/' + channel + '.py'
+    output = []
+    if '아이템 = 1' in open(path).read():
+          f=open(path)
+          
+          for line in f:
+            if not '아이템 = 1' in line:
+                output.append(line)
+          f.close()
+          f = open(path, 'w')
+          f.writelines(output)
+          f.write('아이템 = 0')
+          f.close()
+          
+          await ctx.send('아이템 모드가 꺼졌습니다.')
+    elif '아이템 = 0' in open(path).read():
+        f = open(path)
+        
+        for line in f:
+            if not '아이템 = 0' in line:
+                output.append(line)
+        f.close()
+        f = open(path, 'w')
+        f.writelines(output)
+        f.write('아이템 = 1')
+        f.close()
+        
+        await ctx.send('아이템 모드가 켜졌습니다.')
+        
 @bot.command(pass_context=True)
 async def 오퍼링(ctx):
-	id = (ctx.message.author.mention)
-	msg = ctx.message.content
-	msg = msg.replace(".오퍼링 ", "")
-	
-	if msg in ".오퍼링":
-		await ctx.send(file=discord.File("/app/offering/o" + str(random.randint(1, 16)) + ".jpg"))
-	else:
-		if int(msg) > 5:
-			await ctx.send(format(id) + '오퍼링은 최대 5개 까지 가능합니다.')
-		else:
-			for i in range(0, int(msg)):
-				await asyncio.sleep(0.3)
-				await ctx.send(file=discord.File("/app/offering/o" + str(random.randint(1, 16)) + ".jpg"))
-			
+    id = (ctx.message.author.mention)
+    msg = ctx.message.content
+    msg = msg.replace(".오퍼링 ", "")
+    
+    if msg in ".오퍼링":
+        await ctx.send(file=discord.File("/app/offering/o" + str(random.randint(1, 16)) + ".jpg"))
+    else:
+        if int(msg) > 5:
+            await ctx.send(format(id) + '오퍼링은 최대 5개 까지 가능합니다.')
+        else:
+            for i in range(0, int(msg)):
+                await asyncio.sleep(0.3)
+                await ctx.send(file=discord.File("/app/offering/o" + str(random.randint(1, 16)) + ".jpg"))
+            
 @bot.command(pass_context=True)
 async def 컨셉퍽(ctx):
-	id = (ctx.message.author.mention)
-	a = random.randrange(0, 9)
-	
-	await ctx.send( format(id) + "님의 컨셉퍽은 " + "**```cs\n" +  scperk.scperk[a] +"입니다.\n```**")
+    id = (ctx.message.author.mention)
+    a = random.randrange(0, 9)
+    
+    await ctx.send( format(id) + "님의 컨셉퍽은 " + "**```cs\n" +  scperk.scperk[a] +"입니다.\n```**")
 
 @bot.command(pass_context=True)
 async def 지연시간(ctx):
-	ping = (int)(bot.latency*1000)
-	await ctx.send(str(ping) + "ms")
+    ping = (int)(bot.latency*1000)
+    await ctx.send(str(ping) + "ms")
 
 access_token = os.environ["BOT_TOKEN"]
 bot.run(access_token)
